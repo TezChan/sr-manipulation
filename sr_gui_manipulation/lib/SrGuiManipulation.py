eade98d4a56828b6e8f8b504aa2855621ae38224
Mark Addison <markpitchless.shadow@gmail.com>
2012-02-28 12:04:35 +0000

    Wire up detect_objects call, no UI yet.

--- sr_gui_manipulation/lib/SrGuiManipulation.py
+++ sr_gui_manipulation/lib/SrGuiManipulation.py
@@ -18,18 +18,46 @@ import os
 
 import roslib; roslib.load_manifest('sr_gui_manipulation')
 import rospy
-from rospy import loginfo
+from rospy import loginfo, logerr, logdebug
 
 from rosgui.QtBindingHelper import loadUi
 from QtCore import QEvent, QObject, Qt, QTimer, Slot, QThread, SIGNAL, QPoint
 from QtGui import QDockWidget, QShortcut, QMessageBox, QFrame, QHBoxLayout, QCheckBox, QLabel, QCursor, QColor, QMessageBox
 
+from tabletop_object_detector.srv import TabletopDetection
+from tabletop_collision_map_processing.srv import TabletopCollisionMapProcessing
+from household_objects_database_msgs.srv import GetModelDescription
+
+class TableObject(object):
+    """
+    Contains all the relevant info for an object.
+    A map of TableObject is stored in the ObjectSelection plugin.
+    """
+    def __init__(self):
+        self.graspable_object = None
+        self.graspable_object_name = None
+        self.model_description = None
+
+class Model(object):
+    def __init__(self):
+        self.name = None
+
 class SrGuiManipulation(QObject):
 
     def __init__(self, parent, plugin_context):
         super(SrGuiManipulation, self).__init__(parent)
-        self.setObjectName('SrGuiManipulation')
 
+        self.setObjectName('SrGuiManipulation')
+        # Set by call to process_collision_map()
+        self.collision_support_surface_name = None
+        self.raw_objects            = None
+        self.found_objects          = {}
+        self.unknown_object_counter = 1 #starts at 1 as it's only used for display
+        self.service_tabletop_collision_map   = None
+        self.service_db_get_model_description = None
+        self.service_object_detector          = None
+
+        # UI setup
         ui_file = os.path.join(
                 os.path.dirname(os.path.realpath(__file__)),
                 '../ui/SrGuiManipulation.ui')
@@ -46,7 +74,26 @@ class SrGuiManipulation(QObject):
         self.win.installEventFilter(self)
 
         # Bind button clicks
-        #self.win.btn_select_all.pressed.connect(self.on_select_all_pressed)
+        self.win.btn_detect_objects.pressed.connect(self.detect_objects)
+        self.win.btn_collision_map.pressed.connect(self.process_collision_map)
+        self.win.btn_collision_map.setEnabled(False)
+
+        # Service setup
+        srvname = '/tabletop_collision_map_processing/tabletop_collision_map_processing'
+        rospy.wait_for_service(srvname)
+        self.service_tabletop_collision_map = rospy.ServiceProxy(srvname, TabletopCollisionMapProcessing)
+
+        srvname = 'object_detection'
+        rospy.wait_for_service(srvname)
+        self.service_object_detector = rospy.ServiceProxy(srvname, TabletopDetection)
+
+        srvname = 'objects_database_node/get_model_description'
+        rospy.wait_for_service(srvname)
+        self.service_db_get_model_description = rospy.ServiceProxy(srvname, GetModelDescription)
+
+    def hello(self):
+        """Say hello, useful for quick test of buttons"""
+        loginfo("hello")
 
     def eventFilter(self, obj, event):
         if obj is self.win and event.type() == QEvent.Close:
@@ -65,4 +112,65 @@ class SrGuiManipulation(QObject):
 
     def restore_settings(self, global_settings, perspective_settings):
         loginfo(self.objectName()+" restoring settings")
+
+    def detect_objects(self):
+        self.found_objects.clear()
+        try:
+            self.raw_objects = self.service_object_detector(True, True, 1)
+        except rospy.ServiceException, e:
+            print "Service did not process request: %s" % str(e)
+
+        # Take a new collision map + add the detected objects to the collision
+        # map and get graspable objects from them
+        tabletop_collision_map_res = self.process_collision_map()
+        if tabletop_collision_map_res != 0:
+           for grasp_obj, grasp_obj_name in zip(tabletop_collision_map_res.graspable_objects, tabletop_collision_map_res.collision_object_names):
+               #loginfo("Found: {0} {1}".format(grasp_obj,grasp_obj_name))
+               loginfo("Found: %s" % grasp_obj_name)
+               obj_tmp = TableObject()
+               obj_tmp.graspable_object = grasp_obj
+               obj_tmp.graspable_object_name = grasp_obj_name
+
+               if len(grasp_obj.potential_models) > 0:
+                   model_index = grasp_obj.potential_models[0].model_id
+               else:
+                   model_index = -1
+               obj_tmp.model_description = self.get_object_name(model_index)
+
+               self.found_objects[obj_tmp.model_description.name] = obj_tmp
+
+        if self.raw_objects != None:
+           self.win.btn_collision_map.setEnabled(True)
+        #self.parent.parent.reload_object_signal_widget.reloadObjectSig['int'].emit(1)
+
+    def process_collision_map(self):
+        res = 0
+        try:
+            #Args: detection_result reset_collision_models reset_attached_models desired_frame
+            res = self.service_tabletop_collision_map.call(self.raw_objects.detection, True, True, "/fixed")
+        except rospy.ServiceException, e:
+            logerr("Service did not process request: %s" % str(e))
+
+        if res != 0:
+            loginfo("collision_support_surface_name: "+res.collision_support_surface_name)
+            self.collision_support_surface_name = res.collision_support_surface_name
+        return res
+
+    def get_object_name(self, model_id):
+        """
+        return the object name given its index (read from database, or
+        create unique name if unknown object).
+        """
+        model = Model()
+        #todo make sure name is unique
+        if model_id == -1:
+            model.name = "unknown_" + str(self.unknown_object_counter)
+            self.unknown_object_counter += 1
+        else:
+            try:
+                model = self.service_db_get_model_description(model_id)
+            except rospy.ServiceException, e:
+                print "Service did not process request: %s" % str(e)
+                model.name = "unkown_recognition_failed"
+        return model
 
