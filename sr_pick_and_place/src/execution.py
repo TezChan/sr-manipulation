231c0907c0006b5af1a46225ca8c55060afb90b2
guihome@wanadoo.fr <>
2012-10-19 17:36:49 +0200

    Added pre-grasp and grasp to pick action (no reactive mode yet)

--- sr_pick_and_place/src/execution.py
+++ sr_pick_and_place/src/execution.py
@@ -21,13 +21,16 @@ import rospy
 
 from object_manipulation_msgs.msg import Grasp, PickupGoal, PickupAction, PlaceGoal, PlaceAction
 from object_manipulation_msgs.srv import GraspPlanning, GraspPlanningRequest, GraspPlanningResponse
+from object_manipulation_msgs.msg import GraspHandPostureExecutionAction ,GraspHandPostureExecutionGoal
 from arm_navigation_msgs.srv import GetMotionPlanRequest, GetMotionPlanResponse, FilterJointTrajectory, FilterJointTrajectoryRequest
 from arm_navigation_msgs.msg import DisplayTrajectory, JointLimits
 from geometry_msgs.msg import PoseStamped
+from actionlib_msgs.msg import GoalID, GoalStatus, GoalStatusArray
 from trajectory_msgs.msg import JointTrajectory
 from sr_utilities.srv import getJointState
 from planification import Planification
 from sr_utilities.srv import getJointState
+import actionlib
 
 import time
 import copy
@@ -56,19 +59,22 @@ class Execution(object):
         self.get_joint_state_ = rospy.ServiceProxy("/getJointState", getJointState)
         self.trajectory_filter_ = rospy.ServiceProxy("/trajectory_filter_unnormalizer/filter_trajectory", FilterJointTrajectory)
         self.grasp_planning_service_ = rospy.ServiceProxy("/objects_database_node/database_grasp_planning", GraspPlanning)
+        self.hand_posture_execution_actionclient_ = actionlib.SimpleActionClient('/right_arm/hand_posture_execution', GraspHandPostureExecutionAction)
+        self.hand_posture_execution_actionclient_.wait_for_server()
+        rospy.loginfo("hand_posture_execution server ready")
 
                 
     def pick(self,pickup_goal):
         #get grasps for the object
-          # fill up a grasp planning request
+        # fill up a grasp planning request
         grasp_planning_req = GraspPlanningRequest()
         grasp_planning_req.arm_name = pickup_goal.arm_name
         grasp_planning_req.target = pickup_goal.target     
 
-         # call grasp planning service
+        # call grasp planning service
         grasp_planning_res = self.grasp_planning_service_.call(grasp_planning_req) 
         #print grasp_planning_res
-         # process grasp planning result
+        # process grasp planning result
         if (grasp_planning_res.error_code.value != grasp_planning_res.error_code.SUCCESS):
             rospy.logerr("No grasp found for this object, we will generate some, but only when the node is ready for that !")
             return 
@@ -77,32 +83,29 @@ class Execution(object):
                 
         #for each grasp, check path from pre-grasp pose to grasp pose first and then check motion to pre-grasp pose
         motion_plan_res=GetMotionPlanResponse()
+        grasp_to_execute_=Grasp()
         for index, grasp in enumerate(grasp_planning_res.grasps):
             # extract grasp_pose
             grasp_pose_ = PoseStamped()
             grasp_pose_.header.frame_id = "/world";
             grasp_pose_.pose = grasp.grasp_pose
            
-            #pre_grasp_pose_ = PoseStamped()
-            #pre_grasp_pose_.header.frame_id = "/world";
-            #pre_grasp_pose_.pose.position=grasp_pose_.pose.position
-            #pre_grasp_pose_.pose.orientation=grasp_pose_.pose.orientation
-            
+            # copy the grasp_pose as a pre-grasp_pose
             pre_grasp_pose_ = copy.deepcopy(grasp_pose_)
             
             # add desired_approach_distance along the approach vector. above the object to plan pre-grasp pose
 
-            # currently add this to Z because approach vector needs to be computer somehow first
-            pre_grasp_pose_.pose.position.z = pre_grasp_pose_.pose.position.z + 0.08 
+            # currently add this to Z because approach vector needs to be computed somehow first (TODO)
+            pre_grasp_pose_.pose.position.z = pre_grasp_pose_.pose.position.z + 0.05 
           
-            
             # for distance from 0 (grasp_pose) to desired_approach distance (pre_grasp_pose) test IK/Collision and save result
-            # decompose this in 10 steps by default
+            # decompose this in X steps depending on distance to do and max speed
             interpolated_motion_plan_res = self.plan.get_interpolated_ik_motion_plan(pre_grasp_pose_, grasp_pose_, False)
               
             # check the result (depending on number of steps etc...)
             if (interpolated_motion_plan_res.error_code.val == interpolated_motion_plan_res.error_code.SUCCESS):
                 number_of_interpolated_steps=0
+                # check if one approach trajectory is feasible
                 for interpolation_index, traj_error_code in enumerate(interpolated_motion_plan_res.trajectory_error_codes):
                     if traj_error_code.val!=1:
                         rospy.logerr("One unfeasible approach-phase step found at "+str(interpolation_index)+ "with val " + str(traj_error_code.val))
@@ -110,6 +113,7 @@ class Execution(object):
                     else:
                         number_of_interpolated_steps=interpolation_index
                 
+                # if trajectory is feasible then plan reach motion to pre-grasp pose
                 if number_of_interpolated_steps+1==len(interpolated_motion_plan_res.trajectory.joint_trajectory.points):
                     rospy.loginfo("Grasp number "+str(index)+" approach is possible, checking motion plan to pre-grasp")
                     #print interpolated_motion_plan_res
@@ -117,32 +121,49 @@ class Execution(object):
                     # check and plan motion to this pre_grasp_pose
                     motion_plan_res = self.plan.plan_arm_motion( pickup_goal.arm_name, "jointspace", pre_grasp_pose_ )
         
-                    #if one is successful do not test others
+                    #if this pre-grasp pose is successful do not test others
                     if (motion_plan_res.error_code.val == motion_plan_res.error_code.SUCCESS):
                         rospy.loginfo("Grasp number "+str(index)+" is possible, executing it")
+                        # copy the grasp to execute for the following steps
+                        grasp_to_execute_ = copy.deepcopy(grasp)
                         break
                 else:
                     rospy.logerr("Grasp number "+str(index)+" approach is impossible")
-                    #print interpolated_motion_plan_res
+                    print interpolated_motion_plan_res
             else:
                 rospy.logerr("Grasp number "+str(index)+" approach is impossible")
                 #print interpolated_motion_plan_res
-            
-        #if one is successful, compute approach (from pre-grasp to grasp)
+        
+        
+        # check and plan motion to this pre_grasp_pose
+        #motion_plan_res = self.plan.plan_arm_motion( pickup_goal.arm_name, "jointspace", pre_grasp_pose_ )
+        
         if (motion_plan_res.error_code.val == motion_plan_res.error_code.SUCCESS):
             #put hand in pre-grasp posture
-
+            if self.pre_grasp_exec(grasp_to_execute_)<0:
+                QMessageBox.warning(self, "Warning",
+                    "Pre-grasp action failed: ")
+                return
+                    
             #go there 
             # filter the trajectory
             filtered_traj = self.filter_traj_(motion_plan_res)
 
             self.display_traj_( filtered_traj )
+            
+            # reach pregrasp pose
             self.send_traj_( filtered_traj )
-
-            #approach
-            time.sleep(15)
+            time.sleep(20) # TODO use actionlib here
+            
+            # approach 
             self.send_traj_( interpolated_motion_plan_res.trajectory.joint_trajectory )
+            time.sleep(10) # TODO use actionlib here
+            
             #grasp
+            if self.grasp_exec(grasp_to_execute_)<0:
+                QMessageBox.warning(self, "Warning",
+                    "Grasp action failed: ")
+                return
         else:
             rospy.logerr("None of the grasps tested is possible")
               
@@ -152,8 +173,42 @@ class Execution(object):
         #check if object is grasped
         #compute straight trajectory in 6D or in 3D if it fails
     
-   
+    def pre_grasp_exec(self, grasp,timeout_sec=10.0):
+      
+        hand_posture_goal_ = GraspHandPostureExecutionGoal()
+        hand_posture_goal_.grasp = grasp
+        # do pre-grasp action
+        hand_posture_goal_.goal = GraspHandPostureExecutionGoal.PRE_GRASP
+        hand_posture_goal_.max_contact_force = 0.0
+        # call hand_posture_exec action lib
+        rospy.loginfo("Trying to pre-grasp...")
+        return self.hand_posture_exec(hand_posture_goal_,timeout_sec)
+    
+    def grasp_exec(self, grasp, timeout_sec=15.0):
+    
+        hand_posture_goal_ = GraspHandPostureExecutionGoal()
+        hand_posture_goal_.grasp = grasp
+        # do grasp action
+        hand_posture_goal_.goal = GraspHandPostureExecutionGoal.GRASP
+        hand_posture_goal_.max_contact_force = 1.0
+        # call hand_posture_exec action lib
+        rospy.loginfo("Trying to grasp...")
+        return self.hand_posture_exec(hand_posture_goal_,timeout_sec)
         
+    def hand_posture_exec(self, hand_posture_goal,timeout_sec):
+        # call hand_posture_exec action lib
+        self.hand_posture_execution_actionclient_.send_goal(hand_posture_goal)
+        # wait for result up to 10 seconds
+        self.hand_posture_execution_actionclient_.wait_for_result(timeout=rospy.Duration.from_sec(timeout_sec))
+        #rospy.loginfo("Got Hand Posture Exec results")
+        # analyze the result
+        hand_posture_exec_result_ = self.hand_posture_execution_actionclient_.get_result()
+        if self.hand_posture_execution_actionclient_.get_state() != GoalStatus.SUCCEEDED:
+            rospy.logerr("The action has failed: " + str(hand_posture_exec_result_.result) )
+            return -1
+        else:
+            rospy.loginfo("The action has succeeded")
+            return 0
                 
     def display_traj_(self, trajectory):
         print "Display trajectory"
